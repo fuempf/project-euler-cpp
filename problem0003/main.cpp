7e11ca466695971f1b0ed7257866f9f29a6a0b00
Karol Actun <karol.actun@uni-duesseldorf.de>
2021-01-15 22:14:59 +0100

    changed calc_prime func

--- problem0003/main.cpp
+++ problem0003/main.cpp
@@ -3,31 +3,34 @@
 
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 #include <cmath>
 #include <chrono>
 
 typedef long long int lli;
 
+
 std::vector<lli> calculate_primes_up_to(lli limit){
     // vector for primes
     std::vector<lli> primes;
-    std::unordered_set<lli> not_primes;
-    not_primes.insert(0);
-    not_primes.insert(0);
+    bool not_primes[limit];
+    for(int i = 0; i < limit; i++) not_primes[i] = false;
+    not_primes[0] = true;
+    not_primes[1] = true;
     for(lli i = 2; i < limit; i++){
         // continue if already marked as not prime
-        if(not_primes.find(i) != not_primes.end()) continue;
+        if(not_primes[i]){
+            continue;
+        } 
         //push new prime 
         primes.push_back(i);
-        //mark all non primes
+
+        //mark all non prime
         for(lli j = 2; j*i < limit; j++){
-            not_primes.insert(i*j);
+            not_primes[i*j] = true;
         }
     }
     return primes;
 }
-
 
 int main(){
     auto start = std::chrono::high_resolution_clock::now();
--- problem0007/main.cpp
+++ problem0007/main.cpp
@@ -3,7 +3,6 @@
 
 #include <chrono>
 #include <vector>
-#include <unordered_set>
 #include <iostream>
 #include <cmath>
 
@@ -12,17 +11,21 @@ typedef long long int lli;
 std::vector<lli> calculate_primes_up_to(lli limit){
     // vector for primes
     std::vector<lli> primes;
-    std::unordered_set<lli> not_primes;
-    not_primes.insert(0);
-    not_primes.insert(0);
+    bool not_primes[limit];
+    for(int i = 0; i < limit; i++) not_primes[i] = false;
+    not_primes[0] = true;
+    not_primes[1] = true;
     for(lli i = 2; i < limit; i++){
         // continue if already marked as not prime
-        if(not_primes.find(i) != not_primes.end()) continue;
+        if(not_primes[i]){
+            continue;
+        } 
         //push new prime 
         primes.push_back(i);
-        //mark all non primes
+
+        //mark all non prime
         for(lli j = 2; j*i < limit; j++){
-            not_primes.insert(i*j);
+            not_primes[i*j] = true;
         }
     }
     return primes;
@@ -44,7 +47,7 @@ int main(){
     std::vector<lli> primes = calculate_n_primes(n);
     std::cout << "calculated " << primes.size() << " primes" << std::endl;
     //just making sure the nth prime was calculated...
-    if(primes.size() > n) result = primes[n];
+    if(primes.size() > n) result = primes[n-1]; // starts with 0 so, index n-1 is nth prime 
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "result: " << result << std::endl;
     std::cout << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
